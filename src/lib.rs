c05b096f1bab3249d9102f89b28e224fda963359
Alistair Francis <alistair@alistair23.me>
2020-01-03 22:31:10 -0800

    Initial commit
    Signed-off-by: Alistair Francis <alistair@alistair23.me>

---
+++ src/lib.rs
@@ -0,0 +1,29 @@
+/*
+ * Copyright 2020 Alistair Francis <alistair@alistair23.me>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#[repr(C)] pub struct ClutterActor { _private: [u8; 0] }
+
+/// ChamplainView functions
+#[link(name = "champlain-0.12")]
+extern {
+    fn champlain_view_new () -> *mut ClutterActor;
+}
+
+pub fn view_new() -> *mut ClutterActor {
+    unsafe {
+        champlain_view_new()
+    }
+}
